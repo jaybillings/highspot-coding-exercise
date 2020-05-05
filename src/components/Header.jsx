5a4d5ed1a6d9acdf0193448a60392222fc10a3e9
Jay Billings <contact@scribbleowl.com>
2020-05-05 15:51:22 -0700

    More visual spiff-ups
    * Adds text gradient to header
    * Card outline slightly more visible

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "../styles/header.scss";
 
 export default function Header(props) {
-  return <header className={'main-header container'}>
-    <h1>Elder Scrolls Legends<br/> Card Browser</h1>
+  /**
+   * @var dividedHeader - A nested JSX object which allows CSS to apply text gradient to each character
+   *
+   * Lines are wrapped in <p>
+   *   Words are wrapped in <div>
+   *     Characters are wrapped in <span>
+   */
+  const dividedHeader = ['Elder Scrolls Legends', 'Card Browser'].map(line => {
+    return <p>{line.split(' ').map(word => {
+      return <div>{word.split('').map(char => <span>{char}</span>)}</div>
+    })}</p>
+  });
+  return <header className={'main-header'}>
+    <h1>{dividedHeader}</h1>
   </header>
 }
