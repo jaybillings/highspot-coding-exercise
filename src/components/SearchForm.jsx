da0846b2ed991a317cde4b65c2fc638f0afc2555
Jay Billings <contact@scribbleowl.com>
2020-05-04 22:31:44 -0700

    Search form style spiff up

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "../styles/search-form.scss"
+import { GiCancel } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
 
 export default class SearchForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.inputRef = React.createRef();
-
-    this.state = {
-      searchTerm: this.props.searchTerm
-    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -25,12 +23,14 @@ export default class SearchForm extends React.Component {
   }
 
   render() {
-    return <div className={'search-container container'}>
+    return <div className={'search-form-wrapper container'}>
       <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
         <label htmlFor={'name-input'}><span className={'sr-only'}>Search cards by full or partial name:</span></label>
-        <input type={'text'} name={'name-input'} ref={this.inputRef} placeholder={'Search card names'} />
-        <button type={'submit'}>Search</button>
-        <button type={'reset'}>Reset Search</button>
+        <div className={'fake-input'}>
+          <input type={'text'} name={'name-input'} ref={this.inputRef} placeholder={'Search card names'} />
+          <button type={'submit'} title={'Search cards'}><FaSearch /></button>
+          <button type={'reset'} title={'Reset search results'}><GiCancel /></button>
+        </div>
       </form>
       <p>Showing {this.props.resultsCount} cards of {this.props.allResultsCount} </p>
     </div>
